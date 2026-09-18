519cc9f0064aaf6759684003f23bde06c985f953
waaaaaaario <amartucakovic2010@gmail.com>
2026-09-17 22:52:52 -0400

    Sync Codeforces: 282A - Bit++ [C++23 (GCC 14-64, msys2)]

---
+++ 282A_Bit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin>>n;
+	int x = 0;
+	for(int i = 0; i<n; i++)
+	{
+		string s;
+		cin >> s;
+		if (s[1]=='+')
+		x++;
+		else
+		x--;
+	}
+	cout << x << "\n";
+	return 0;
+}
