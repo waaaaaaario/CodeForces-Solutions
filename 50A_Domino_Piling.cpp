a4aecef4e0bea315269a6a0371bcd3bcd1fd9f36
waaaaaaario <amartucakovic2010@gmail.com>
2026-09-19 23:21:05 -0400

    Add solution for Codeforces problem 50A
    Implement solution for the Domino Piling problem.

---
+++ 50A_Domino_Piling.cpp
@@ -0,0 +1,38 @@
+// Problem: A. Domino piling
+// Contest: Codeforces - Codeforces Beta Round 47
+// URL: https://codeforces.com/problemset/problem/50/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <utility>
+ 
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+ 
+void solve() {
+    int m, n;
+    cin >> m >> n;
+    int x = (m*n)/2;
+    cout << x << "\n";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
